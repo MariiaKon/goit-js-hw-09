b6b8a3781d8658101bec23ff030b22b90ac650d9
Mariia Konovalchuk <71904680+MariiaKon@users.noreply.github.com>
2022-01-21 21:51:44 +0200

    task 9.1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+stopBtn.disabled = true;
+startBtn.addEventListener('click', switchColor);
+stopBtn.addEventListener('click', stopSwitchColor);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function switchColor() {
+  interval = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+}
+
+function stopSwitchColor() {
+  clearInterval(interval);
+
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}
